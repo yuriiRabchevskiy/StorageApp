c47aaad9b35e199be2ab533ce7f5cb8208391d5e
yuriy.horak <yhorak@blackthorn-vision.com>
2021-11-29 22:05:24 +0200

    work with optional columns in progress

---
+++ src/app/controls/columns-picker/columns-picker.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ITableColumn } from '@app/models/component/list-api.component';
+
+@Component({
+  selector: 'app-columns-picker',
+  templateUrl: './columns-picker.component.html',
+  styleUrls: ['./columns-picker.component.scss']
+})
+export class ColumnsPickerComponent implements OnInit {
+
+  private _columns: ITableColumn[];
+  public get columns() { return this._columns; }
+  @Input() public set columns(value: ITableColumn[]) {
+    this._columns = value;
+  }
+
+  private _hiddenColumns: Set<string>;
+  public hiddenCopy: Set<string>;
+
+  public get hiddenColumns() { return this._hiddenColumns; }
+  @Input() public set hiddenColumns(value: Set<string>) {
+    this._hiddenColumns = value;
+    this.hiddenCopy = new Set(value);
+  }
+
+  @Output() closeDialog: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onColumnChange(column: ITableColumn, args: InputEvent) {
+    const checkbox = args.target as HTMLInputElement;
+    const field = column.field;
+    if (checkbox.checked) {
+      this.hiddenCopy.delete(field);
+    } else {
+      this.hiddenCopy.add(field);
+    }
+  }
+
+  close() {
+    this.closeDialog.emit(false);
+  }
+
+  save() {
+    this.hiddenColumns.clear();
+    this.hiddenCopy.forEach(el => this._hiddenColumns.add(el));
+    this.closeDialog.emit(true);
+  }
+
+}
--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShareModule } from '../shared/share.module';
-import { SpinnerComponent, ConfirmComponent, IconButtonComponent,
-         OrderEditorComponent, ButtonGroupComponent } from './index';
+import {
+  SpinnerComponent, ConfirmComponent, IconButtonComponent,
+  OrderEditorComponent, ButtonGroupComponent
+} from './index';
 import { UserEditorComponent } from './user-editor/user-editor.component';
 import { ReturnComponent } from './return/return.component';
+import { ColumnsPickerComponent } from './columns-picker/columns-picker.component';
 
 @NgModule({
   declarations: [SpinnerComponent, ConfirmComponent, IconButtonComponent, OrderEditorComponent,
-                 ButtonGroupComponent, UserEditorComponent, ReturnComponent],
+    ButtonGroupComponent, UserEditorComponent, ReturnComponent, ColumnsPickerComponent],
 
   imports: [CommonModule, ShareModule],
   entryComponents: [],
   exports: [SpinnerComponent, ConfirmComponent, IconButtonComponent, OrderEditorComponent,
-            ButtonGroupComponent, UserEditorComponent, ReturnComponent]
+    ButtonGroupComponent, UserEditorComponent, ReturnComponent, ColumnsPickerComponent]
 })
 
 export class ControlsModule {
