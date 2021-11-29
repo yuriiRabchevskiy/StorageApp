8dfefb2dda0ca21c1552299c0a6e80119072ecf6
yuriy.horak <yhorak@blackthorn-vision.com>
2021-11-29 22:05:06 +0200

    work with dates as UTC in progress

--- backend/Storage/DataAccess/ApplicationDbContext.cs
+++ backend/Storage/DataAccess/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 using DataAccess.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DataAccess
 {
@@ -17,6 +18,25 @@ namespace DataAccess
       // Customize the ASP.NET Identity model and override the defaults if needed.
       // For example, you can rename the ASP.NET Identity table names and more.
       // Add your customizations after calling base.OnModelCreating(builder);
+      builder.Entity<OrderAction>()
+        .Property(x => x.Date)
+        .HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+      builder.Entity<ProductAction>()
+        .Property(x => x.Date)
+        .HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+      builder.Entity<Order>()
+        .Property(x => x.OpenDate)
+        .HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+      builder.Entity<Order>()
+        .Property(x => x.CanceledDate)
+        .HasConversion(v => v, v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null);
+
+      builder.Entity<Order>()
+        .Property(x => x.CloseDate)
+        .HasConversion(v => v, v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null);
     }
 
     public DbSet<Category> Categories { get; set; }
@@ -27,8 +47,6 @@ namespace DataAccess
     public DbSet<WarehouseProducts> WarehouseProducts { get; set; }
     public DbSet<Preference> UserPreferences { get; set; }
     public DbSet<OrderAction> OrderAction { get; set; }
-
     
-
   }
 }
