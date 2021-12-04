904f5f9ad28efe4ccf785c292c252d7c48dc1900
yuriy.horak <yhorak@blackthorn-vision.com>
2021-12-04 22:27:21 +0200

    Added export service to export products store state

--- backend/Storage/BusinessLogic/Models/Api/ApiProduct.cs
+++ backend/Storage/BusinessLogic/Models/Api/ApiProduct.cs
@@ -18,4 +18,18 @@ namespace BusinessLogic.Models.Api
 
     public Dictionary<string, int> Balance { get; set; }
   }
+
+  public class CsvProduct
+  {
+    public int Id { get; set; }
+    public int CategoryId { get; set; }
+    public string ProductType { get; set; } // e.g. backpack, stairs
+    public string Model { get; set; } // e.g. backpack, stairs
+    public string Producer { get; set; }
+    public string Color { get; set; }
+    public string Size { get; set; }
+    public string FreeNote { get; set; }
+    public bool IsActive { get; set; }
+    public int TotalBalance { get; set; }
+  }
 }
