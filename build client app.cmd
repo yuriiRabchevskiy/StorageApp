c23e39d920a3da46e8c7dd4442b4649a9266a16c
Yurii Horak <yuriy.horak@blackthorn-vision.com>
2023-02-13 12:35:12 +0200

    fixed phone format, moved sms message to config

--- build client app.cmd
+++ build client app.cmd
@@ -1,7 +1,7 @@
 cd %1
 RMDIR "backend\Storage\Storage\wwwroot" /S /Q
 
-node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng build --prod ---progress true
+node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng build --configuration production ---progress true
 
 robocopy dist backend\Storage\Storage\wwwroot /S /PURGE /XD .svn /XF *.scss  *.cs *.pdb *.map *.user *.csproj /NFL /NJH /NP 
 IF %ERRORLEVEL% LSS 8 goto finish
